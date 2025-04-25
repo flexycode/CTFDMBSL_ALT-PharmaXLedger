563a97fb96d56321e4ca620330a9d36746ec6824
Jay <flexyledger@gmail.com>
2025-04-26 03:10:05 +0800

    📓 Dev Delete

--- supabase/migrations/20240420000002_add_payment_details_to_orders.sql
+++
@@ -1,9 +0,0 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1 FROM information_schema.columns 
-        WHERE table_name = 'orders' AND column_name = 'subtotal'
-    ) THEN
-        ALTER TABLE orders ADD COLUMN subtotal numeric(12,2);
-    END IF;
-END $$;
